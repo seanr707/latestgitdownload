97243ad0abd77e16b451b3ce859520e369aa7dd7
seanr707 <seanr.git@gmail.com>
2016-09-03 23:52:08 -0400

    Fixed missing port issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const get = require('simple-get');
 const app = express();
 
-app.set('port'), (process.env.PORT || 5000);
+app.set('port', (process.env.PORT || 5000));
 
 // Root path for users who are looking into project
 app.get('/', (req, res) => {
